60c512d62d99e50a08673b59bc82cbabaa4101ec
piotr.knapczyk <piotr.knapczyk@me.com>
2017-03-26 21:50:45 +0200

    login zamiast username

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var auth = require('./auth.js');
 /*
  * Routes that can be accessed by any one
  */
+router.post('/register', require('./register').register);
 router.post('/login', auth.login);
 
 module.exports = router;
---
+++ routes/register.js
@@ -0,0 +1,55 @@
+var jwt = require('jwt-simple');
+
+var register = {
+
+  register: function(req, res) {
+
+
+    var username = req.body.login || '';
+    var password = req.body.password || '';
+
+    if (username == '' || password == '') {
+      res.status(401);
+      res.json({
+        "status": 401,
+        "message": "Invalid credentials"
+      });
+      return;
+    }
+
+    // Fire a query to your DB and check if the credentials are valid
+    var response = auth.validate(username, password);
+   
+    if (!response) { // If authentication fails, we send a 401 back
+      res.status(401);
+      res.json({
+        "status": 401,
+        "message": "Invalid credentials 2"
+      });
+      return;
+    }
+
+    if (response) {
+
+      // If authentication is success, we will generate a token
+      // and dispatch it to the client
+
+      res.json(genToken(response));
+    }
+
+  },
+  validate: function(username, password) {
+    // spoofing the DB response for simplicity
+    var response = {  
+      success: 'true'
+    };
+
+    return response;
+  },
+
+
+
+}
+
+
+module.exports = register;
