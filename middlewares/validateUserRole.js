24554340ab9a47e3d50cfd4dd4f90841aaa01fbe
bialylis <piotr.knapczyk@me.com>
2017-05-15 11:44:34 +0200

    zmiana 2

--- middlewares/validateUserRole.js
+++ middlewares/validateUserRole.js
@@ -1,32 +1,35 @@
 module.exports = function(req, res, next) {
 
-	var client = req.app.get('db');
-   	var user = req.user;
+  var client = req.app.get('db');
+    var user = req.user;
     var groupid = req.params.id;
 
-console.log("userid " + user.user_id)
+    console.log("validation")
 
     var query = client.query(`SELECT * FROM "group" WHERE admin = ($1) AND "group_id" = ($2)
-                                `, [user.user_id, groupid]);
+                                UNION
+                               SELECT group_id, name, admin, secret_word FROM "group" INNER JOIN membership ON "group".group_id = membership."group"
+                                WHERE membership.member = ($1) AND membership.accepted = TRUE AND membership."group" = ($2)`, [user.user_id, groupid]);
 
     query.on("row", function(result){
-    	// console.log(result);
-    	req.group = result;
-   		next();
+      console.log(result);
+      req.group = result;
+      next();
     });
     query.on("end", function(result){
-      console.log(result)
-          if(result.rowCount == 0){
-                  res.status(401);
-                  res.json({
-                    'status':401,
-                    "message": "This user does not have access to this group"
-                  })
+      if (result.rowCount == 0) {
+        res.status(401);
+        res.json({
+          'status':401,
+          "message": "This user does not have access to this group"
+        })
 
-          }
+      }
     })
     query.on("error", function(result){
-    	res.status(401);
+       console.log("validation")
+
+      res.status(401);
         res.json({
           'status':401,
           "message": "This user does not have access to this group"
@@ -35,4 +38,3 @@ console.log("userid " + user.user_id)
 
 
 };
-
--- routes/user_managment.js
+++ routes/user_managment.js
@@ -1,82 +1,111 @@
 var user_managment = {
-	invite: function(req, res){
-		var client = req.app.get('db');
-    	var user = req.user;
+    invite: function(req, res){
+        var client = req.app.get('db');
+        var user = req.user;
 
-    	var group = req.group;
+        var group = req.group;
 
-    	var invited = req.params.userid;
-    	var isAdmin = group.admin == user.user_id;
+        var email = req.body.invited_email || '';
 
-    	if (isAdmin) {
+        var isAdmin = group.admin == user.user_id;
+        console.log("invite")
+        if (isAdmin) {
 
-    		var query = client.query(`INSERT INTO "membership" ("group", member, accepted) VALUES ($1, $2, $3)`, [group.group_id ,invited, true])
-    		query.on('error', function(result){
-    			console.log(result);
-	    		res.status(400);
-		        res.json({
-		            "status": 400,
-		            "message": "Can not invite user"
-		        });
-    		})
+            getIdForEmail(client, email, function(id){
+                console.log(id)
+                if (id == null) {
+                    res.status(400);
+                    res.json({
+                        "status": 400,
+                        "message": "Can not invite user"
+                    });
+                    return;
+                }
+                var query = client.query(`INSERT INTO "membership" ("group", member, accepted) VALUES ($1, $2, $3)`, [group.group_id ,id, true])
+                query.on('error', function(result){
+                    console.log(result);
+                    res.status(400);
+                    res.json({
+                        "status": 400,
+                        "message": "Can not invite user " + id
+                    });
+                })
+                query.on("end", function(result){
+                    res.status(200);
+                    res.json({
+                        "status": 200,
+                        "message": "User invited to the group"
+                    })
+                })
+            })
+        }else {
 
-    		query.on("end", function(result){
-    			res.status(200);
-    			res.json({
-    				"status": 200,
-    				"message": "User invited to the group"
-    			})
-    		})
-    	}else {
+            res.status(401);
+            res.json({
+                "status": 401,
+                "message": "User doesnt have permission to invite other users"
+            });
 
-    		res.status(401);
-	        res.json({
-	            "status": 401,
-	            "message": "User doesnt have permission to invite other users"
-	        });
+        }
 
-    	}
+    },
+    remove: function(req, res){
 
-	},
-	remove: function(req, res){
+        var client = req.app.get('db');
+        var user = req.user;
 
-		var client = req.app.get('db');
-    	var user = req.user;
+        var group = req.group;
 
-    	var group = req.group;
+        var invited = req.params.userid;
+        var isAdmin = group.admin == user.user_id;
 
-    	var invited = req.params.userid;
-    	var isAdmin = group.admin == user.user_id;
+        if (isAdmin) {
 
-    	if (isAdmin) {
+            var query = client.query(`DELETE FROM "membership" WHERE "group" = $1 AND member = $2`, [group.group_id ,invited])
+            query.on('error', function(result){
+                console.log(result);
+                res.status(400);
+                res.json({
+                    "status": 400,
+                    "message": "Can not remove user"
+                });
+            })
 
-    		var query = client.query(`DELETE FROM "membership" WHERE "group" = $1 AND member = $2`, [group.group_id ,invited])
-    		query.on('error', function(result){
-    			console.log(result);
-	    		res.status(400);
-		        res.json({
-		            "status": 400,
-		            "message": "Can not remove user"
-		        });
-    		})
+            query.on("end", function(result){
+                res.status(200);
+                res.json({
+                    "status": 200,
+                    "message": "User removed from the group"
+                })
+            })
+        }else {
 
-    		query.on("end", function(result){
-    			res.status(200);
-    			res.json({
-    				"status": 200,
-    				"message": "User removed from the group"
-    			})
-    		})
-    	}else {
+            res.status(401);
+            res.json({
+                "status": 401,
+                "message": "User doesnt have permission to remove other users"
+            });
 
-    		res.status(401);
-	        res.json({
-	            "status": 401,
-	            "message": "User doesnt have permission to remove other users"
-	        });
+        }
+    }
+}
 
-    	}
-	}
+function getIdForEmail(client, email, next){
+    var query = client.query(`SELECT user_id FROM user_account WHERE email = $1`, [email])
+
+    query.on('error', function(result) {
+        next(null)
+    })
+
+    query.on('row', function (result) {
+        next(result.user_id)
+    })
+
+    query.on('end', function (result) {
+        if (result.rowCount == 0) {
+            next(null)
+        }
+    })
 }
 
 module.exports = user_managment;
