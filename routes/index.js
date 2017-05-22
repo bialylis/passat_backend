ba162f31eeb369eb416abb1661c5b6ac2706296a
skalawi <skalawiktor@gmail.com>
2017-05-22 09:37:49 +0200

    Passwords database

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var auth = require('./auth.js');
 var groups = require('./groups.js');
 var managment = require('./user_managment.js');
 var crypto = require('./crypto.js');
-
+var password = require('./password');
 
 /*
  * Routes that can be accessed by any one
@@ -20,6 +20,10 @@ router.use('/auth/group/:id', [require('../middlewares/validateUserRole.js')]);
 router.get('/auth/group', groups.getAll);
 router.get('/auth/group/:id', groups.getOne);
 
+router.post('/auth/group/:id/password', password.add_group_password);
+
+router.get('/auth/group/:id/password', password.get_group_passwords);
+router.get('/auth/group/:id/password/:passid', password.get_group_password);
 
 router.post('/auth/group', groups.create);
 router.post('/auth/group/:id', groups.update);
---
+++ routes/password.js
@@ -0,0 +1,125 @@
+var password = {
+
+    add_group_password: function(req, res){
+        var client = req.app.get('db');
+        var group_id = req.params.id;
+        var login = req.body.encrypted_login;
+        var pass = req.body._encrypted_password;
+        var note = req.body.note;
+
+        addPassword(client, login, pass, note, null, group_id, function(success){
+            console.log("add password")
+            if (success) {
+                var response = {
+                    success: 'true'
+                };
+                res.json(response);
+            }else {
+                res.status(400);
+                res.json({
+                    'status':400,
+                    "message": "Database error"
+                })
+
+            }
+        })
+    },
+
+    get_group_password: function(req, res){
+        var client = req.app.get('db');
+        var pass_id = req.params.passid;
+
+        getPassword(client, pass_id, function (response) {
+            if (response == null) {
+                res.status(400);
+                res.json({
+                    "status": 400,
+                    "message": "Can not find password"
+                });
+                return;
+            }
+            res.json(response);
+        })
+
+    },
+
+    get_group_passwords: function(req, res){
+        var client = req.app.get('db');
+        var group_id = req.params.id;
+
+        getPasswords(client, group_id, null, function (response) {
+            console.log("getting passwords finished");
+            res.json(response)
+        })
+    }
+}
+
+function getPasswords(client, group_id, user, next){
+    if (user == null){
+        var query = client.query(`SELECT pass_id from stored_password WHERE "group" = $1`, [group_id]);
+
+        passwords = []
+        query.on('error', function (error) {
+            console.log(error);
+            next(error)
+        })
+
+        query.on('row', function (result) {
+            passwords.push(result);
+        })
+
+        query.on('end', function (result) {
+            next(passwords)
+        })
+    }
+
+}
+
+function addPassword(client, login, pass, note, owner, group, done){
+    if (group==null){
+        var query = client.query(`INSERT INTO stored_password (login, password, note, owner)
+         VALUES ($1, $2, $3, $4)`, [login, pass, note, owner]);
+
+        query.on('error', function (error) {
+            console.log(error);
+            done(false)
+        })
+
+        query.on('end', function (result) {
+            console.log("end");
+            done(true)
+        })
+    }
+    else{
+        var query = client.query(`INSERT INTO stored_password (login, password, note, group)
+         VALUES ($1, $2, $3, $4)`, [login, pass, note, group]);
+
+        query.on('error', function(error){
+            console.log(error);
+            done(false)
+        })
+
+        query.on('end', function(result){
+            console.log("end");
+            done(true)
+        })
+
+    }
+
+}
+
+function getPassword(client, pass_id, next) {
+    var query = client.query(`SELECT * FROM stored_password WHERE pass_id = $1`, [pass_id]);
+
+    query.on('error', function (error) {
+        next(error)
+    })
+    query.on('row', function (result) {
+        next(result)
+    })
+    query.on('end', function (result) {
+        if (result.rowCount == 0){
+            next(null)
+        }
+    })
+}
